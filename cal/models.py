b45971ae29e503c26b5f21bfb65562755e98af51
Anupa Shyamlal <anupa.shyamlal@gmail.com>
2019-01-21 17:16:16 +0800

    model add

---
+++ cal/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Numbers(models.Model):
+    date1 = models.DateField(verbose_name = "Published Date1",null=True,blank=True)
+    date2 = models.DateField(verbose_name = "Published Date2",null=True,blank=True)
+
+
+    def __str__(self):
+        return self.date1,self.date2
